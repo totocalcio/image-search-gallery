6aa2020b8fa7c1fb5110a0e97077a1488f7791a9
totocalcio <totohyper@yahoo.co.jp>
2022-03-16 21:53:22 +0900

    コメントを追加
    - ref属性を用いている部分でTypeScriptエラーが発生している
    - useRefの使い方がそもそも間違っている可能性もある
    - 同ファイル内でわざわざ引数によって受け渡しをしているのはコンポーネント分割を行う予定があるため

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,10 @@ function App() {
     <div className="search">
       <Label>
         Image Search
+        {/* 
+          型 'HTMLInputElement' を型 'LegacyRef<HTMLInputElement> | undefined' に割り当てることはできません。
+          予期された型は、型 'DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>' に対してここで宣言されたプロパティ 'ref' から取得されています
+        */}
         <input
           ref={inputRef}
           type="text"
@@ -33,6 +37,10 @@ function App() {
 
   return (
     <div className="App">
+      {/* 
+        型 '{ ref: RefObject<HTMLInputElement>; }' を型 'IntrinsicAttributes & HTMLInputElement' に割り当てることはできません。
+        プロパティ 'ref' は型 'IntrinsicAttributes & HTMLInputElement' に存在しません。 
+      */}
       <Search ref={inputRef} />
       <Image />
     </div>
